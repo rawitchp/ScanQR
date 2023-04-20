d3957fbb498100a85d0f81cba068ce958da09900
Rawitch Payakkawan <rawitch13@gmail.com>
2023-04-20 10:17:22 +0700

    delete idle import

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
